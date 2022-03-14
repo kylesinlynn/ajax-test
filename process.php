c3c8678c7f996f7cd1a2c82bef98856642b521af
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-03-14 08:12:48 +0630

    added form data into database

--- process.php
+++ process.php
@@ -1,5 +1,8 @@
 <?php
 
+// Connect to database
+$conn = mysqli_connect('localhost', 'root', '', 'ajax_test');
+
 echo 'Processing...';
 
 // Check for GET variable
@@ -9,5 +12,14 @@ if (isset($_GET['name'])) {
 
 // Check for POST variable
 if (isset($_POST['name'])) {
-    echo 'Your name is ' . $_POST['name'];
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    // echo 'Your name is ' . $_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)) {
+        echo 'User Added...';
+    } else {
+        echo 'ERROR: ' . mysqli_error($conn);
+    }
 }
